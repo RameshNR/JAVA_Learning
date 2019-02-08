f1acb408a90695b9d226eed1e8b7ae38e4a397b6
RameshNR <47139284+RameshNR@users.noreply.github.com>
2019-02-08 09:31:48 +0530

    java learning
    Array list demo

---
+++ ArrayListDemo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ArrayListDemo
+{
+public static void main(String arg[])
+{
+ArrayList list=new ArrayList();
+list.add("Ramesh");
+list.add("Soniya");
+list.add("Jaya");
+list.add("Priya");
+list.add("Pooja");
+System.out.println(list);
+}
+}
